7fe8dd265eff41a94937acf6f41937c99cb81b3c
andre <andres.mateus01@uptc.edu.co>
2023-11-06 13:18:27 -0500

    separacion del trabajo

--- main.c
+++ main.c
@@ -1,6 +1,70 @@
 #include <stdio.h>
 
+void numeroRomano();
+
+void factoresPrimos();
+
+void nombresPropios();
+
+void numerosEgolatras();
+
+void numerosAmigos();
+
+void fechas();
+
+void productopunto();
+
+void multiplicacionMatrices();
+
+void matrizMagica();
+
 int main() {
     printf("Hello, World!\n");
     return 0;
 }
+
+/* Responsable alejandro*/
+void numeroRomano(){
+
+}
+
+/* Responsable andres*/
+void factoresPrimos(){
+
+}
+
+/* Responsable andres*/
+void nombresPropios(){
+
+}
+
+/* Responsable alejandro*/
+void numerosEgolatras(){
+
+}
+
+/* Responsable andres*/
+void numerosAmigos(){
+
+}
+
+/* Responsable andres*/
+void fechas(){
+
+}
+
+/* Responsable alejandro*/
+void productopunto(){
+
+}
+
+/* Responsable andres*/
+void multiplicacionMatrices(){
+
+}
+
+/* Responsable dumar*/
+void matrizMagica(){
+
+}
+
