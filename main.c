dd7e82339684e20a0c3c66ba4c1a253e6655df3b
andre <andres.mateus01@uptc.edu.co>
2023-11-06 13:36:16 -0500

    arreglos menores
    Co-Authored-By: Dumar Malpica <108911234+Dumarr@users.noreply.github.com>

--- main.c
+++ main.c
@@ -19,12 +19,15 @@ void multiplicacionMatrices();
 void matrizMagica();
 
 int main() {
+    system("cls");
     int opcion;
     do {
         printf("Bienvenido al programa \nSelecciones una opcion: "
            "\n1. Numero Romano \n2. Factores Primos \n3. Nombres Propios \n4. Numeros Egolatras \n5. Numeros Amigos "
            "\n6. Fechas \n7. Producto Punto \n8. Multiplicacion de Matrices \n9. Matriz Magica \n10. Salir \n");
         scanf("%d", &opcion);
+        // limpiar el escaner
+        fflush(stdin);
         switch (opcion) {
             case '1':
                 numeroRomano();
@@ -53,8 +56,11 @@ int main() {
             case '9':
                 matrizMagica();
                 break;
+            case '10':
+                printf("Gracias por usar el programa");
+                break;
             default:
-                printf("Opcion no valida");
+                printf("Opcion no valida\n");
                 break;
     }
     } while (opcion != 10);
