56523cf1bb68bb410355dbc8fcddfeaaac7ee8c7
andre <andres.mateus01@uptc.edu.co>
2023-11-06 13:31:30 -0500

    Implementacio menu
    aqui implemebtamos la logica basica del menu
    
    Co-Authored-By: Dumar Malpica <108911234+Dumarr@users.noreply.github.com>

--- main.c
+++ main.c
@@ -19,7 +19,45 @@ void multiplicacionMatrices();
 void matrizMagica();
 
 int main() {
-    printf("Hello, World!\n");
+    int opcion;
+    do {
+        printf("Bienvenido al programa \nSelecciones una opcion: "
+           "\n1. Numero Romano \n2. Factores Primos \n3. Nombres Propios \n4. Numeros Egolatras \n5. Numeros Amigos "
+           "\n6. Fechas \n7. Producto Punto \n8. Multiplicacion de Matrices \n9. Matriz Magica \n10. Salir \n");
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case '1':
+                numeroRomano();
+                break;
+            case '2':
+                factoresPrimos();
+                break;
+            case '3':
+                nombresPropios();
+                break;
+            case '4':
+                numerosEgolatras();
+                break;
+            case '5':
+                numerosAmigos();
+                break;
+            case '6':
+                fechas();
+                break;
+            case '7':
+                productopunto();
+                break;
+            case '8':
+                multiplicacionMatrices();
+                break;
+            case '9':
+                matrizMagica();
+                break;
+            default:
+                printf("Opcion no valida");
+                break;
+    }
+    } while (opcion != 10);
     return 0;
 }
 
